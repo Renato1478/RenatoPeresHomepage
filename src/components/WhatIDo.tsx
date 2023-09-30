8ccbba85b8e32a2138e3586a0309eb87ef79c0bf
Renato1478 <renatoperesdesouza@hotmail.com>
2023-09-30 13:13:55 -0300

    Improving what i do? session. -> I don't like borders, used different background to highlight the content

--- src/components/WhatIDo.tsx
+++ src/components/WhatIDo.tsx
@@ -9,7 +9,7 @@ export default function WhatIDo() {
       {/* Card List */}
       <div className="flex max-md:flex-col pt-12 justify-center gap-x-12">
         {/* Design */}
-        <div className="mt-8 max-md:mt-0 h-60 flex flex-col w-1/3 max-md:w-full p-6 border-solid border-2 border-zinc-300 hover:border-purple-400 dark:border-zinc-800 dark:hover:border-purple-400 transition ease-in-out duration-300">
+        <div className="mt-8 max-md:mt-0 h-60 flex flex-col w-1/3 max-md:w-full p-6 rounded-md bg-zinc-100 dark:bg-opacity-5">
           <div className="flex gap-x-2 align-middle">
             <div className="text-purple-600">
               <DesignIcon />
@@ -20,7 +20,7 @@ export default function WhatIDo() {
             <h2 className="text-zinc-700 dark:text-white">
               Things I enjoy designing:
             </h2>
-            <ul className="space-y-1 text-zinc-500 dark:text-zinc-500 list-disc list-inside">
+            <ul className="space-y-1 pt-3 text-zinc-500 dark:text-zinc-500 list-disc list-inside">
               <li>Apps</li>
               <li>Websites</li>
               <li>Logos</li>
@@ -29,7 +29,7 @@ export default function WhatIDo() {
           </div>
         </div>
         {/* Frontend */}
-        <div className="flex h-60 max-md:mt-4 flex-col w-1/3 max-md:w-full p-6 border-solid border-2 transition ease-in-out border-zinc-300 hover:border-violet-400 dark:border-zinc-800 dark:hover:border-violet-400 duration-300">
+        <div className="flex h-60 max-md:mt-4 flex-col w-1/3 max-md:w-full p-6 rounded-md bg-zinc-100 dark:bg-opacity-5">
           <div className="flex gap-x-2 align-middle">
             <div className="text-violet-600">
               <FrontendIcon />
@@ -40,7 +40,7 @@ export default function WhatIDo() {
             <h2 className="text-zinc-700 dark:text-white">
               Frameworks I usually work with:
             </h2>
-            <ul className="space-y-1 text-zinc-500 dark:text-zinc-500 list-disc list-inside">
+            <ul className="space-y-1 pt-3 text-zinc-500 dark:text-zinc-500 list-disc list-inside">
               <li>Next.js</li>
               <li>Three.js / WebGL</li>
               <li>React Native</li>
@@ -49,7 +49,7 @@ export default function WhatIDo() {
           </div>
         </div>
         {/* Backend */}
-        <div className="mt-8 max-md:mt-4 h-60 flex flex-col w-1/3 max-md:w-full p-6 border-solid border-2 transition ease-in-out border-zinc-300 hover:border-red-400 dark:border-zinc-800 dark:hover:border-red-400 duration-300">
+        <div className="mt-8 max-md:mt-4 h-60 flex flex-col w-1/3 max-md:w-full p-6 rounded-md bg-zinc-100 dark:bg-opacity-5">
           <div className="flex gap-x-2 align-middle">
             <div className="text-red-400">
               <Backend />
@@ -60,7 +60,7 @@ export default function WhatIDo() {
             <h2 className="text-zinc-700 dark:text-white">
               Some tools I&apos;m used to:
             </h2>
-            <ul className="space-y-1 text-zinc-500 dark:text-zinc-500 list-disc list-inside">
+            <ul className="space-y-1 pt-3 text-zinc-500 dark:text-zinc-500 list-disc list-inside">
               <li>NodeJS</li>
               <li>Laravel</li>
               <li>PostgresSQL</li>
